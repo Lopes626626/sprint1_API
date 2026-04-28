1edf5f0912fdac4666323ac51aa1c3c809421f68
Rafael <benedetti.rafael626@gmail.com>
2026-04-28 14:36:14 -0300

    feat: aplicando a ocorrencia no repositorio

--- src/main/java/com/fiap/sprint1/backend_consultas/repository/OcorrenciaRepository.java
+++ src/main/java/com/fiap/sprint1/backend_consultas/repository/OcorrenciaRepository.java
@@ -1,4 +1,9 @@
 package com.fiap.sprint1.backend_consultas.repository;
 
-public class OcorrenciaRepository {
+import com.fiap.sprint1.backend_consultas.model.Ocorrencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Long> {
 }
