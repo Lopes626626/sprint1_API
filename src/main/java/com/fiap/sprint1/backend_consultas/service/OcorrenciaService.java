1d0472c5faf6d17ba5530cf788671a88177f96c1
Rafael <benedetti.rafael626@gmail.com>
2026-04-28 14:37:37 -0300

    feat: aplicando a ocorrencia no service

---
+++ src/main/java/com/fiap/sprint1/backend_consultas/service/OcorrenciaService.java
@@ -0,0 +1,45 @@
+package com.fiap.sprint1.backend_consultas.service;
+
+import com.fiap.sprint1.backend_consultas.model.Ocorrencia;
+import com.fiap.sprint1.backend_consultas.repository.OcorrenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OcorrenciaService {
+
+    @Autowired
+    private OcorrenciaRepository repository;
+
+    public Ocorrencia criar(Ocorrencia ocorrencia) {
+        return repository.save(ocorrencia);
+    }
+
+    public List<Ocorrencia> listarTodas() {
+        return repository.findAll();
+    }
+
+    public Optional<Ocorrencia> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public Ocorrencia atualizar(Long id, Ocorrencia ocorrenciaAtualizada) {
+        return repository.findById(id).map(ocorrencia -> {
+            ocorrencia.setTipoDefeito(ocorrenciaAtualizada.getTipoDefeito());
+            ocorrencia.setLinhaProducao(ocorrenciaAtualizada.getLinhaProducao());
+            ocorrencia.setStatus(ocorrenciaAtualizada.getStatus());
+            return repository.save(ocorrencia);
+        }).orElse(null);
+    }
+
+    public boolean deletar(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
